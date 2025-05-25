83bac07eb388dd97f4da94331f6713013b7bf4ee
Priyanshu Singh <in.priyanshusingh@gmail.com>
2025-05-25 17:08:16 +0530

    Add Test.java and modify greeting message in Main.java
    Created a new Test.java file to demonstrate basic data types with examples. Changed `System.out.printf` to `System.out.print` in Main.java for simpler output formatting.

---
+++ src/com/priyanshusingh/corejava/datatypes/Test.java
@@ -0,0 +1,32 @@
+package com.priyanshusingh.corejava.datatypes;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        //Integral Numbers
+        System.out.println("Byte Range: ");
+        System.out.println("------------------");
+        byte byteDataType = 122;
+        System.out.println("Example byte value: " + byteDataType);
+        System.out.println("Minimum byte value: " + Byte.MIN_VALUE);
+        System.out.println("Maximum byte value: " + Byte.MAX_VALUE);
+
+        int integerDataType = 434;
+        short shortDataType = 443;
+        long longDataType = 3094030580325083505L;
+
+        //Decimal Numbers
+        float floatDataType = 43.354F;
+        double doubleDataType = 442.5434343434343434;
+
+        //Boolean
+        boolean isAdult = false;
+
+        //Characters
+        char gradeCharacter = 'A';
+
+
+
+    }
+}
